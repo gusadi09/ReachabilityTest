066626ce6a08ea22a2a0d63b3b026011ff95ad8c
gusadi09 <gusadi872@gmail.com>
2021-02-15 17:20:09 +0800

    Reachability adding for test connection

--- ReachabilityTest/ViewController.swift
+++ ReachabilityTest/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import Reachability
 
 class ViewController: UIViewController {
+    
+    let reachability = try! Reachability()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        reachability.whenReachable = { _ in
+            DispatchQueue.main.async {
+                        self.view.backgroundColor = UIColor.green
+                    }
+        }
+        
+        reachability.whenUnreachable = { _ in
+            DispatchQueue.main.async {
+                        self.view.backgroundColor = UIColor.red
+                    }
+        }
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(internetChanged), name: Notification.Name.reachabilityChanged , object: reachability)
+            do{
+                try reachability.startNotifier()
+            } catch {
+                print("Could not strat notifier")
+            }
     }
 
+    @objc  func internetChanged(note:Notification)  {
+        let reachability  =  note.object as! Reachability
+        if reachability.connection != .unavailable{
+            if reachability.connection == .wifi{
+                self.view.backgroundColor = UIColor.green
+            }
+            else{
+                DispatchQueue.main.async {
+                    self.view.backgroundColor = UIColor.orange
+                }
+            }
+        } else{
+            DispatchQueue.main.async {
+                self.view.backgroundColor = UIColor.red
+            }
+        }
+    }
 
 }
 
